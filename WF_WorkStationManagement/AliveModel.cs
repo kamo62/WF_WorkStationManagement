dfc47dd8b7244830582cc21536170213f3a3fad1
Kamogelo Mashishi <kamo.mash@outlook.com>
2018-05-21 21:02:13 +0200

    Update to make port configurable and sql

---
+++ WF_WorkStationManagement/AliveModel.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_WorkStationManagement
+{
+
+        [Table("Users")]
+        public class AliveModel
+        {
+            [Key]
+            public string hostname { get; set; }
+            public string ipAddress { get; set; }
+            public string datetime { get; set; }
+            public string processName { get; set; }
+            public string port { get; set; }
+            [ReadOnly(true)]
+            public DateTime CreatedDate { get; set; }
+        }
+
+    }
+
--- WF_WorkStationManagement/Program.cs
+++ WF_WorkStationManagement/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Linq;
+using System.Net;
 
 namespace WF_WorkStationManagement
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServerConnString"].ConnectionString);
+            foreach (NetStatPortsAndProcessNames.Port s in NetStatPortsAndProcessNames.GetNetStatPorts())
+            {
+                String portNumber = ConfigurationManager.AppSettings["port"];
+                if (s.port_number.Equals(portNumber))
+                {
+                    string computerName = Dns.GetHostName();
+                    var newId = db.Insert(new AliveModel
+                    {
+                        hostname = computerName,
+                        ipAddress = Dns.GetHostEntry(computerName).AddressList[2].ToString(),
+                        port = s.port_number,
+                        processName = s.name
+                    });
+                }
+            }
         }
     }
 }
